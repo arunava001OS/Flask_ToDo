330c1c51956c4a468e294eef1f3de250dc8130a0
arunava001OS <arunavasarkar3@gmail.com>
2020-10-04 20:21:01 +0530

    comments

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+## IMPORTS
 from flask import Flask,render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+## APP INITIALIZATION
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
+## DATEBASE CONFIGURATIONS
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key= True)
     content = db.Column(db.String(200),nullable=False)
     completed = db.Column(db.Integer,default=0)
     date_created = db.Column(db.DateTime,default=datetime.utcnow)
+
+## ROUTE FUNCTIONS
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -50,7 +54,7 @@ def update(id):
     else:
         return render_template('update.html',task = task_to_update)
 
-
+## RUN COMMAND
 if __name__ == "__main__":
     app.run(debug=True)
 
